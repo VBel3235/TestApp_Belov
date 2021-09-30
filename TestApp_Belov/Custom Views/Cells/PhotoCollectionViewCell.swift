9795357c9b300d6e502b0d2e23f8b3953703dd08
VBel3235 <Vlad95.0753@gmail.com>
2021-09-30 22:39:33 +1000

    refactoring

--- TestApp_Belov/Custom Views/Cells/PhotoCollectionViewCell.swift
+++ TestApp_Belov/Custom Views/Cells/PhotoCollectionViewCell.swift
@@ -21,8 +21,6 @@ class PhotoCollectionViewCell: UICollectionViewCell {
         fatalError("init(coder:) has not been implemented")
     }
     
-    
-    
     private func configure(){
         addSubview(photoImageView)
         
